e72a2aacd45ea0c163995bc5e9b1dbb941efaf75
ubuntupunk <ubuntupunk@gmail.com>
2025-07-04 11:01:25 +0000

    export wp xml

--- wp/web/app/jetpack-waf/bootstrap.php
+++ wp/web/app/jetpack-waf/bootstrap.php
@@ -6,5 +6,6 @@ define( 'JETPACK_WAF_SHARE_DATA', false );
 define( 'JETPACK_WAF_SHARE_DEBUG_DATA', false );
 define( 'JETPACK_WAF_DIR', '/home/sexthera/public_html/muizenmesh.co.za/wp/web/app/jetpack-waf' );
 define( 'JETPACK_WAF_WPCONFIG', '/home/sexthera/public_html/muizenmesh.co.za/wp/web/app/../wp-config.php' );
+define( 'JETPACK_WAF_ENTRYPOINT', 'rules/rules.php' );
 require_once '/home/sexthera/public_html/muizenmesh.co.za/wp/web/app/plugins/jetpack/vendor/autoload.php';
 Automattic\Jetpack\Waf\Waf_Runner::initialize();
